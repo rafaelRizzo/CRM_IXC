6b7b5a6af5e5b6c56adc8263936ae9722bf9801f
Rafael Rizzo <92104771+rafaelRizzo@users.noreply.github.com>
2023-02-26 21:35:29 -0300

    query empresa

---
+++ gets/empresa.php
@@ -0,0 +1,11 @@
+<?php
+include_once("../../db/config.php");
+
+$sql = $pdo->prepare("SELECT * FROM empresas WHERE status != 0");
+$sql->execute();
+
+if ($sql->rowCount() > 0) {
+    echo json_encode($sql->fetchAll(PDO::FETCH_ASSOC));
+} else {
+    echo json_encode('false');
+}
